fa591e64098cfb4115392d1cb8b5190fadac12c6
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-16 14:24:16 -0300

    Account bank program  exercise
    I will be uploding as I do it.

---
+++ Encapsulamento/Conta_bancária/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=-1;
+            
+            //Menu principal
+            while  (i != 0){
+                Console.Clear();
+                Console.WriteLine("Welcome to the account bank program");
+
+                Console.WriteLine("Selecione alguma opção:");
+                Console.WriteLine(" ");
+                Console.WriteLine("1-Abrir conta\n2-Sacar\n3-Depositar\n0-Sair");
+                i= int.Parse(Console.ReadLine());
+
+                //opções selecionadas do menú:
+                if (i == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the account bank program");
+                    Console.WriteLine("Abrindo");
+                    Console.ReadKey();
+                }
+                else if (i == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the account bank program");
+                    Console.WriteLine("Sacando");
+                    Console.ReadKey();
+                }
+                else if (i == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the account bank program");
+                    Console.WriteLine("Depositando");
+                    Console.ReadKey();
+                }
+                else if (i == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Exiting");
+                    Console.ReadKey();
+                }
+                else {
+                    Console.Clear();
+                    Console.WriteLine("Comando inválido! Tente novamente!!");
+                    //Thread.Sleep(3000);
+                    //Console.Clear();
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
