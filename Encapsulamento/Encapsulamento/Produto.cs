4634188b0d3ddffe702616be9311a0a954ef04a5
David-Ramalho <86934295+David-Ramalho@users.noreply.github.com>
2022-08-16 00:11:01 -0300

    Some training

---
+++ Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,74 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        //Construtor padrão
+        public Produto() { }
+
+        // Construtor 
+        public Produto(string nome, double preco, int quantidade) 
+        {
+            _quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            if (quantidade >= 1)
+            {
+                _quantidade += quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida");                
+                
+            }
+
+        }
+
+        public void SetAdicionarProdutos(int quantidade)
+        {
+            if (quantidade >= 1)
+            {
+                _quantidade += quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida");
+
+            }
+
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
