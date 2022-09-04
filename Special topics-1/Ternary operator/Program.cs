c0868b9a600f029d8fc1cdb265f3835d044a6023
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-04 16:38:46 -0300

    Ternary and switch case

---
+++ Special topics-1/Ternary operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("             ***Welcome to the discount Card program***");
+            Console.WriteLine("\nType the products value: ");
+            double price= double.Parse(Console.ReadLine());
+
+            double discount = (price < 20) ? price * 0.1 : price * 0.05;
+            Console.WriteLine("\nDiscount: " + discount+"$");
+
+        }
+    }
+}
