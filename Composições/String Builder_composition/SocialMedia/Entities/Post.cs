7746f04eac83fa7f4c224b2b2b218c8c03c89be2
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-18 18:31:26 -0300

    SocialMedia

---
+++ Composições/String Builder_composition/SocialMedia/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Entities
+{
+     class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string content { get; set; }
+        public int Likes { get; set; }
+    }
+}
