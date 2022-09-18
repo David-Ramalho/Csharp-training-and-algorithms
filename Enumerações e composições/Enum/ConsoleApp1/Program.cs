6609c13fa4598032be6d17a7f5ccbd12eee67293
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-18 12:59:24 -0300

    Work

--- Enumerações e composições/Enum/ConsoleApp1/Program.cs
+++ Enumerações e composições/Enum/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.Enums;
 
 namespace ConsoleApp1
 {
@@ -14,28 +16,37 @@ namespace ConsoleApp1
                 Console.WriteLine("                   ***Welcome to the Workers controller***");
                 Console.WriteLine("\nChoose the best option:");
                 Console.WriteLine("1-Register employee\n2-Add contracts\n3-Calculate employee's Income\n0-Exit");
-                i=int.Parse(Console.ReadLine());
+                i = int.Parse(Console.ReadLine());
 
-                if(i == 1)
+                if (i == 1)
                 {
                     Console.Clear();
-                    Console.WriteLine("Resgister");
-                    Console.ReadKey();
+
+                    Console.WriteLine("Enter department's name: ");
+                    string dptName = Console.ReadLine();
+                    Console.WriteLine("Enter Worker Data Below:");
+                    Console.WriteLine("Name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Level(Junior/MidLevel/Senior): ");
+                    WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+                    Console.WriteLine("Base Salary: ");
+                    double baseSalary = double.Parse(Console.ReadLine());
+
                 }
 
-                else if(i == 2)
+                else if (i == 2)
                 {
                     Console.Clear();
                     Console.WriteLine("Adding contracts");
-                    Console.ReadKey();
+
                 }
                 else if (i == 3)
                 {
                     Console.Clear();
                     Console.WriteLine("Calculating income");
-                    Console.ReadKey();
+
                 }
-                else if (i ==0)
+                else if (i == 0)
                 {
                     Console.Clear();
                     Console.WriteLine("Exiting");
