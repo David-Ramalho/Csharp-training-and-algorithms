ab969e9ca7b2e3da60981d851d29f4a8bbe07924
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-18 19:04:22 -0300

    String Builder

--- Composições/String Builder_composition/SocialMedia/Entities/Post.cs
+++ Composições/String Builder_composition/SocialMedia/Entities/Post.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SocialMedia.Entities;
 
 namespace SocialMedia.Entities
 {
-     class Post
+    class Post
     {
         public DateTime Moment { get; set; }
         public string Title { get; set; }
         public string content { get; set; }
         public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            this.content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes- ");
+            sb.AppendLine(Moment.ToString("DD/MM/YYYY HH:mm:ss"));
+            sb.AppendLine(content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment comment in Comments)
+            {
+                sb.AppendLine(comment.Text);
+            }
+            return sb.ToString();
+
+        }
     }
 }
--- Composições/String Builder_composition/SocialMedia/Program.cs
+++ Composições/String Builder_composition/SocialMedia/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using SocialMedia.Entities;
+using System.Globalization;
 
 namespace SocialMedia
 {
@@ -6,7 +8,35 @@ namespace SocialMedia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comment c1 = new Comment("Have a Nice trip");
+            Comment c2 = new Comment("Thats cool");
+
+            Post p1 = new Post(
+                DateTime.ParseExact("21/06/2018 13:05:44", "DD/MM/YYYY HH:mm:ss", CultureInfo.InvariantCulture),
+                "traveling to New Zealand",
+                "I'm going to visit this wonderful country",
+                12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.ParseExact("28/07/2018 23:14:19", "DD/MM/YYYY HH:mm:ss", CultureInfo.InvariantCulture),                    
+                    "Good night guys",
+                    "See you tomorrow",
+                    5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+
+
+
         }
     }
 }
