e93ba2f51c493e105a1042f36d37857e45ab1e24
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-18 12:06:30 -0300

    Update Worker.cs
    Composition

--- Enumerações e composições/Enum/ConsoleApp1/Entities/Worker.cs
+++ Enumerações e composições/Enum/ConsoleApp1/Entities/Worker.cs
@@ -13,12 +13,45 @@ namespace ConsoleApp1.Entities
         public Department Department { get; set; } //Composition
 
         //Composition list type. Workers may have many contracts so we'll be using lists.
-        public List<HourContract> Contracts { get; set; }= new List<HourContract>();
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
 
         public Worker()
         {
             //Basic constructor
         }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year== year && contract.Date.Month== month)
+                {
+                    sum += contract.TotalValue();
+                }
+                }
+            return sum;
+            }
+        }
+
     }
 }
 
