16e7a37dad37a25cbeb1bff8a1ce7cf9f539e3e4
David-Ramalho <tavaresramalho@hotmail.com>
2022-09-18 12:47:25 -0300

    Composition menu

--- Enumerações e composições/Enum/ConsoleApp1/Entities/Worker.cs
+++ Enumerações e composições/Enum/ConsoleApp1/Entities/Worker.cs
@@ -53,5 +53,5 @@ namespace ConsoleApp1.Entities
         }
 
     }
-}
+
 
--- Enumerações e composições/Enum/ConsoleApp1/Program.cs
+++ Enumerações e composições/Enum/ConsoleApp1/Program.cs
@@ -6,7 +6,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            int i = -1;
+            Console.Clear();
+            while (i != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("                   ***Welcome to the Workers controller***");
+                Console.WriteLine("\nChoose the best option:");
+                Console.WriteLine("1-Register employee\n2-Add contracts\n3-Calculate employee's Income\n0-Exit");
+                i=int.Parse(Console.ReadLine());
+
+                if(i == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Resgister");
+                    Console.ReadKey();
+                }
+
+                else if(i == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Adding contracts");
+                    Console.ReadKey();
+                }
+                else if (i == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Calculating income");
+                    Console.ReadKey();
+                }
+                else if (i ==0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Exiting");
+                }
+                else Console.WriteLine("Invalid option!\n try again");
+                Console.ReadKey();
+            }
         }
     }
 }
