af77b874844c93342ed8ce01166b6f03ac6d4152
David-Ramalho <tavaresramalho@hotmail.com>
2022-10-07 21:42:51 -0300

    Create BusinessAccount.cs

---
+++ Inheritance & Polymorphism/BankAccount/ConsoleApp1/Entities/BusinessAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+     class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+        {
+            LoanLimit = loanLimit;
+        }
+    }
+
+ }
