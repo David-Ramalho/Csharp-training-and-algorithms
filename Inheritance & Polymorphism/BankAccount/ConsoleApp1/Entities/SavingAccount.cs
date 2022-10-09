f899fd518ecfb2caa406eec2e53afab07ca2b0d8
David-Ramalho <tavaresramalho@hotmail.com>
2022-10-09 12:21:21 -0300

    downcasting

--- Inheritance & Polymorphism/BankAccount/ConsoleApp1/Entities/SavingAccount.cs
+++ Inheritance & Polymorphism/BankAccount/ConsoleApp1/Entities/SavingAccount.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp1.Entities
 {
-    internal class SavingAccount
+    class SavingAccount : Account
     {
+        public double InterestRate { get; protected set; }
+
+        public SavingAccount()
+        {
+
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
     }
 }
--- Inheritance & Polymorphism/BankAccount/ConsoleApp1/Program.cs
+++ Inheritance & Polymorphism/BankAccount/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleApp1.Entities;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(5000, "Rafael0", 1.0, 200);
+            Account acc3 = new SavingAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
         }
     }
 }
