68e44a6ae73644e26d6f33b2120c47daff71b557
David-Ramalho <tavaresramalho@hotmail.com>
2022-10-30 12:10:20 -0300

    work

--- Inheritance & Polymorphism/Exercises/Products/ConsoleApp1/Program.cs
+++ Inheritance & Polymorphism/Exercises/Products/ConsoleApp1/Program.cs
@@ -9,6 +9,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter the number of products:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.WriteLine("Common, used or imported (c/u/i)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+                if (ch == 'i')
+                {
+                    Console.WriteLine("Customs fee: ");
+                    double fee = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+                    list.Add(new ImportedProduct(name, price, fee));
+                }
+                else if (ch == 'u')
+                {
+                    Console.WriteLine("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+            Console.WriteLine("Products: ");
+            foreach(Product product in list)
+            {
+                Console.WriteLine(product.Name +"$"+product.Price);
+            }
             
         }
     }
