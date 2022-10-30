a23249e4010420a76f48d545347445023936d323
David-Ramalho <tavaresramalho@hotmail.com>
2022-10-30 11:28:56 -0300

    Product

---
+++ Inheritance & Polymorphism/Exercises/Products/ConsoleApp1/Entities/ImportedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customFee)
+            : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+    }
+}
---
+++ Inheritance & Polymorphism/Exercises/Products/ConsoleApp1/Entities/UsedProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price,DateTime manufactureDate)
+        : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+    }
+
+}
