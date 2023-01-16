d830e4afc90422ccce67d3adf9b49d42f79846b8
David-Ramalho <tavaresramalho@hotmail.com>
2023-01-16 18:45:51 -0300

    Perso

---
+++ Try catch and finally/3/ConsoleApp1/Entities/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIN { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIN, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIN = checkIN;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIN);
+        }
+
+    }
+}
