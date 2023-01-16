2ab71e32857fe28c88b7fcc9be7be40a3c9dc26e
David-Ramalho <tavaresramalho@hotmail.com>
2023-01-16 19:06:48 -0300

    Update Reservation.cs

--- Try catch and finally/3/ConsoleApp1/Entities/Reservation.cs
+++ Try catch and finally/3/ConsoleApp1/Entities/Reservation.cs
@@ -1,31 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Course.Entities.Exceptions;
 
-namespace ConsoleApp1.Entities
+namespace Course.Entities
 {
-    internal class Reservation
+    class Reservation
     {
+
         public int RoomNumber { get; set; }
-        public DateTime CheckIN { get; set; }
+        public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
 
         public Reservation()
         {
-
         }
 
-        public Reservation(int roomNumber, DateTime checkIN, DateTime checkOut)
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
             RoomNumber = roomNumber;
-            CheckIN = checkIN;
+            CheckIn = checkIn;
             CheckOut = checkOut;
         }
 
-        public int Duration()
+        public double Duration()
         {
-            TimeSpan duration = CheckOut.Subtract(CheckIN);
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
         }
 
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
     }
 }
