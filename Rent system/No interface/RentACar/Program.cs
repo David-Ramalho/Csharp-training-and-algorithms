0868b47b91165a33c03374408669e62485664585
David-Ramalho <tavaresramalho@hotmail.com>
2023-06-01 13:33:05 -0300

    no interface

--- Rent system/No interface/RentACar/Program.cs
+++ Rent system/No interface/RentACar/Program.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Globalization;
 using RentACar.entities;
+using RentACar.services;
+
 
 Console.WriteLine("Enter a rental data:");
 Console.WriteLine("Car Model: ");
-string model= Console.ReadLine();
+string model = Console.ReadLine();
 Console.WriteLine("Pickup (dd/mm/yyyy hh:mm): ");
-DateTime start= DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 Console.WriteLine("Return (dd/mm/yyyy hh:mm): ");
 DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+Console.WriteLine("Enter price per hour: ");
+double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Enter price per day: ");
+double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-CarRental carRental= new CarRental(start, finish, new Vehicle(model));
 
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+RentalService rentalService = new RentalService(hour, day);
+
+rentalService.ProcessInvoice(carRental);
+
+Console.WriteLine("invoice: ");
+Console.WriteLine(carRental.Invoice);
