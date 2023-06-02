14dabb3e06ddb46f21fcc81a924b8c6db4f2b063
David-Ramalho <tavaresramalho@hotmail.com>
2023-06-02 19:12:54 -0300

    update

--- Rent system/No interface/ConsoleApp1/Program.cs
+++ Rent system/No interface/ConsoleApp1/Program.cs
@@ -1,3 +1,16 @@
 ﻿using System;
 using System.Globalization;
 using ConsoleApp1.entities;
+
+Console.WriteLine("Welcome to you account. Lets talk bussiness");
+Console.WriteLine("Enter account data");
+Console.Write("Number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Holder: ");
+String holder = Console.ReadLine();
+Console.Write("Initial balance: ");
+double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Withdraw limit: ");
+double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Account acc = new Account(number, holder, balance, withdrawLimit);
--- Rent system/No interface/ConsoleApp1/entities/Account.cs
+++ Rent system/No interface/ConsoleApp1/entities/Account.cs
@@ -4,18 +4,31 @@ using System.Globalization;
 
 namespace ConsoleApp1.entities
 {
-    class Account
-    {
-        public int Number { get; set; }
-        public string Holder { get; set; }
-        public double Balance { get; set; }
-        public double WithDrawLimit { get; set; }
+	class Account
+	{
+		public int Number { get; set; }
+		public string Holder { get; set; }
+		private double Balance { get; set; }
+		public double WithDrawLimit { get; set; }
 
+		public Account(int number, string holder, double balance, double withDrawLimit)
 
-        public double deposit(double amount)
-        {
+		{
+			Number = number;
+			Holder = holder;
+			Balance = balance;
+			WithDrawLimit = withDrawLimit;
+		}
+
+		public double deposit(double amount)
+		{
 			return Balance += amount;
 		}
-        }
-    }
+
+		public double withDraw(double amount)
+		{
+			return Balance -= amount;
+		}
+	}
 }
+
