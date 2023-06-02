877c72823f2f84fff404ec70f462f964c5cd3a48
David-Ramalho <tavaresramalho@hotmail.com>
2023-06-02 19:05:02 -0300

    Up

---
+++ Rent system/No interface/ConsoleApp1/entities/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1.entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+
+        public double deposit(double amount)
+        {
+			return Balance += amount;
+		}
+        }
+    }
+}
